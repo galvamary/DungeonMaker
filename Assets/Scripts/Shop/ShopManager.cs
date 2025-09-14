459e5e9b62dade7d12029a8ce5a77da965a81887
galvamary <hoyungbyun@gmail.com>
2025-09-14 18:56:32 +0900

    Monster inventory
    몬스터 인벤토리 패널 제작
    인벤토리 아이템 프리펫 제작
    구매하면 인벤토리에 표시

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -8,6 +8,9 @@ public class ShopManager : MonoBehaviour
     [Header("Shop Settings")]
     [SerializeField] private List<MonsterData> availableMonsters = new List<MonsterData>();
     
+    // 구매한 몬스터 개수 추적
+    private Dictionary<MonsterData, int> ownedMonsters = new Dictionary<MonsterData, int>();
+    
     private void Awake()
     {
         if (Instance == null)
@@ -28,7 +31,24 @@ public class ShopManager : MonoBehaviour
         if (GameManager.Instance.CanAfford(monster.cost))
         {
             GameManager.Instance.SpendGold(monster.cost);
-            Debug.Log($"Purchased {monster.monsterName} for {monster.cost} gold");
+            
+            // 몬스터 개수 업데이트
+            if (ownedMonsters.ContainsKey(monster))
+            {
+                ownedMonsters[monster]++;
+            }
+            else
+            {
+                ownedMonsters[monster] = 1;
+            }
+            
+            // 인벤토리 UI 업데이트
+            if (MonsterInventoryUI.Instance != null)
+            {
+                MonsterInventoryUI.Instance.UpdateMonsterInventory(monster, ownedMonsters[monster]);
+            }
+            
+            Debug.Log($"Purchased {monster.monsterName} for {monster.cost} gold. Total owned: {ownedMonsters[monster]}");
             return true;
         }
         else
@@ -36,6 +56,11 @@ public class ShopManager : MonoBehaviour
             Debug.Log($"Not enough gold to purchase {monster.monsterName}. Cost: {monster.cost}, Current: {GameManager.Instance.CurrentGold}");
             return false;
         }
+    }
+    
+    public int GetMonsterCount(MonsterData monster)
+    {
+        return ownedMonsters.ContainsKey(monster) ? ownedMonsters[monster] : 0;
     }
     
     public List<MonsterData> GetAvailableMonsters()
---
+++ Assets/Scripts/UI/MonsterInventoryUI.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+public class MonsterInventoryUI : MonoBehaviour
+{
+    public static MonsterInventoryUI Instance { get; private set; }
+    
+    [Header("UI References")]
+    [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private Transform inventoryItemContainer;
+    [SerializeField] private GameObject inventoryItemPrefab;
+    
+    private Dictionary<MonsterData, GameObject> inventoryItems = new Dictionary<MonsterData, GameObject>();
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void Start()
+    {
+        if (inventoryPanel != null)
+        {
+            inventoryPanel.SetActive(true);
+        }
+    }
+    
+    public void UpdateMonsterInventory(MonsterData monster, int count)
+    {
+        if (inventoryItems.ContainsKey(monster))
+        {
+            // 이미 있는 몬스터면 개수만 업데이트
+            UpdateInventoryItemCount(inventoryItems[monster], count);
+        }
+        else if (count > 0)
+        {
+            // 새로운 몬스터 추가
+            CreateInventoryItem(monster, count);
+        }
+    }
+    
+    private void CreateInventoryItem(MonsterData monster, int count)
+    {
+        if (inventoryItemPrefab == null || inventoryItemContainer == null) return;
+        
+        GameObject item = Instantiate(inventoryItemPrefab, inventoryItemContainer);
+        SetupInventoryItem(item, monster, count);
+        inventoryItems[monster] = item;
+    }
+    
+    private void SetupInventoryItem(GameObject item, MonsterData monster, int count)
+    {
+        // 몬스터 아이콘 설정
+        Transform iconTransform = item.transform.Find("MonsterIcon");
+        if (iconTransform != null)
+        {
+            Image iconImage = iconTransform.GetComponent<Image>();
+            if (iconImage != null && monster.icon != null)
+            {
+                iconImage.sprite = monster.icon;
+            }
+        }
+        
+        // 개수 텍스트 설정
+        Transform countTransform = item.transform.Find("CountText");
+        if (countTransform != null)
+        {
+            TextMeshProUGUI countText = countTransform.GetComponent<TextMeshProUGUI>();
+            if (countText != null)
+            {
+                countText.text = count.ToString();
+            }
+        }
+    }
+    
+    private void UpdateInventoryItemCount(GameObject item, int count)
+    {
+        if (count <= 0)
+        {
+            // 개수가 0이면 아이템 제거
+            Destroy(item);
+            return;
+        }
+        
+        Transform countTransform = item.transform.Find("CountText");
+        if (countTransform != null)
+        {
+            TextMeshProUGUI countText = countTransform.GetComponent<TextMeshProUGUI>();
+            if (countText != null)
+            {
+                countText.text = count.ToString();
+            }
+        }
+    }
+}
